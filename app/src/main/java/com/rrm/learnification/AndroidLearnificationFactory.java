35f2ee63a64bc6c3d4db09831a77c95b08187df8
Rob Moore <robmoore121@gmail.com>
2019-06-23 21:46:12 +0100

    Extracted some Context usages into an owned class, AndroidPackageContext

--- app/src/main/java/com/rrm/learnification/AndroidLearnificationFactory.java
+++ app/src/main/java/com/rrm/learnification/AndroidLearnificationFactory.java
@@ -1,11 +1,9 @@
 package com.rrm.learnification;
 
-import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.RemoteInput;
 
@@ -19,17 +17,19 @@ class AndroidLearnificationFactory {
     private final String channelId;
     private final Context packageContext;
     private final AndroidLogger androidLogger;
+    private final AndroidPackageContext androidPackageContext;
 
     AndroidLearnificationFactory(Context packageContext, String channelId, AndroidLogger androidLogger) {
         this.channelId = channelId;
         this.packageContext = packageContext;
         this.androidLogger = androidLogger;
+        this.androidPackageContext = new AndroidPackageContext(packageContext);
     }
 
     Notification create(String title, String text) {
         androidLogger.v(LOG_TAG, "Creating a notification with title '" + title + "' and text '" + text + "'");
 
-        String replyLabel = packageContext.getResources().getString(R.string.reply_label);
+        String replyLabel = androidPackageContext.getRemoteInputReplyLabel();
         RemoteInput remoteInput = new RemoteInput.Builder(REPLY_TEXT)
                 .setLabel(replyLabel)
                 .build();
@@ -44,28 +44,22 @@ class AndroidLearnificationFactory {
         // Create the reply action and add the remote input.
         NotificationCompat.Action replyAction = new NotificationCompat.Action.Builder(
                 R.drawable.android_send,
-                packageContext.getString(R.string.reply_label),
+                androidPackageContext.getReplyActionLabel(),
                 replyPendingIntent)
                 .addRemoteInput(remoteInput)
                 .build();
 
         return new NotificationCompat.Builder(packageContext, channelId)
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setLargeIcon(BitmapFactory.decodeResource(packageContext.getApplicationContext().getResources(), R.mipmap.ic_launcher))
+                .setLargeIcon(androidPackageContext.getNotificationLargeIcon())
                 .setContentTitle(title)
                 .setContentText(text)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 // Set the intent that will fire when the user taps the notification
-                .setContentIntent(notificationContentIntent())
+                .setContentIntent(androidPackageContext.notificationContentIntent())
                 .setAutoCancel(true)
                 .addAction(replyAction)
                 .build();
     }
 
-    private PendingIntent notificationContentIntent() {
-        // Create an explicit intent for an Activity in your app
-        Intent intent = new Intent(packageContext, AlertDialog.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        return PendingIntent.getActivity(packageContext, 0, intent, 0);
-    }
 }
---
+++ app/src/main/java/com/rrm/learnification/AndroidPackageContext.java
@@ -0,0 +1,35 @@
+package com.rrm.learnification;
+
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+class AndroidPackageContext {
+    private final Context packageContext;
+
+    public AndroidPackageContext(Context packageContext) {
+        this.packageContext = packageContext;
+    }
+
+    String getRemoteInputReplyLabel() {
+        return packageContext.getResources().getString(R.string.reply_label);
+    }
+
+    String getReplyActionLabel() {
+        return packageContext.getString(R.string.reply_label);
+    }
+
+    Bitmap getNotificationLargeIcon() {
+        return BitmapFactory.decodeResource(packageContext.getApplicationContext().getResources(), R.mipmap.ic_launcher);
+    }
+
+    PendingIntent notificationContentIntent() {
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(packageContext, AlertDialog.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        return PendingIntent.getActivity(packageContext, 0, intent, 0);
+    }
+}
