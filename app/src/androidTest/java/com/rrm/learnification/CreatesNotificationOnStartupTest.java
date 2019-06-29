d6ba7d9a9d9b15b55f98bf186577eafb224b4ecd
Rob Moore <robmoore121@gmail.com>
2019-06-29 15:40:26 +0100

    Make the ui-automator test check that the app creates a notification on start-up.

--- app/src/androidTest/java/com/rrm/learnification/CreatesNotificationOnStartupTest.java
+++ app/src/androidTest/java/com/rrm/learnification/CreatesNotificationOnStartupTest.java
@@ -6,6 +6,7 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.SearchCondition;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.Until;
 
@@ -14,27 +15,38 @@ import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class CreatesNotificationOnStartupTest {
     private static final String PACKAGE_NAME = "com.rrm.learnification";
-    private static final int LAUNCH_TIMEOUT = 5000;
 
     @Test
-    public void canOpenTheApp() {
+    public void whenYouOpenTheAppItCreatesANotification() {
         UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
 
         startApp(device);
+
+        assertNotificationCreated(device);
+    }
+
+    private void assertNotificationCreated(UiDevice device) {
+        int notificationTimeoutMs = 1000;
+        device.openNotification();
+        SearchCondition<Boolean> condition = Until.hasObject(By.pkg(PACKAGE_NAME));
+        assertTrue(device.wait(condition, notificationTimeoutMs));
     }
 
     private void startApp(UiDevice device) {
+        int launchTimeoutMs = 5000;
+
         // Start from the home screen
         device.pressHome();
 
         // Wait for launcher
         final String launcherPackage = device.getLauncherPackageName();
         assertThat(launcherPackage, notNullValue());
-        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), launchTimeoutMs);
 
         // Launch the app
         Context context = InstrumentationRegistry.getInstrumentation().getContext();
@@ -44,6 +56,6 @@ public class CreatesNotificationOnStartupTest {
         context.startActivity(intent);
 
         // Wait for the app to appear
-        device.wait(Until.hasObject(By.pkg(PACKAGE_NAME).depth(0)), LAUNCH_TIMEOUT);
+        device.wait(Until.hasObject(By.pkg(PACKAGE_NAME).depth(0)), launchTimeoutMs);
     }
 }
