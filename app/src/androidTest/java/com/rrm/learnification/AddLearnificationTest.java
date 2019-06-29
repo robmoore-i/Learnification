a04e5d1f800a12a2f36aec53dad16d926a75cc49
Rob Moore <robmoore121@gmail.com>
2019-06-29 09:07:45 +0100

    Wrote an espresso test for adding a learnification to the list view.

---
+++ app/src/androidTest/java/com/rrm/learnification/AddLearnificationTest.java
@@ -0,0 +1,34 @@
+package com.rrm.learnification;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+
+@RunWith(AndroidJUnit4.class)
+public class AddLearnificationTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void typingAAndBIntoTheTextFieldsAndClickingThePlusButtonAddsALearnificationToTheList() {
+        onView(withId(R.id.left_input)).perform(typeText("L"));
+        onView(withId(R.id.right_input)).perform(typeText("R"));
+        onView(withId(R.id.addLearnificationButton)).perform(click());
+
+        onView(withId(R.id.learnificationsListView));
+
+        onData(anything()).inAdapterView(withId(R.id.learnificationsListView)).atPosition(3).check(matches(withText("L - R")));
+    }
+}
