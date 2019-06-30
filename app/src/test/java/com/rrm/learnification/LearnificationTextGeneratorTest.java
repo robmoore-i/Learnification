2c9c63b8b6a92f6d99cc798d567c4b2f208a86aa
Rob Moore <robmoore121@gmail.com>
2019-06-30 20:08:18 +0100

    Delete unused class (LearningItemTemplate)

--- app/src/test/java/com/rrm/learnification/LearnificationTextGeneratorTest.java
+++ app/src/test/java/com/rrm/learnification/LearnificationTextGeneratorTest.java
@@ -3,7 +3,6 @@ package com.rrm.learnification;
 import org.junit.Test;
 
 import java.util.Collections;
-import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,17 +12,11 @@ import static org.mockito.Mockito.when;
 public class LearnificationTextGeneratorTest {
     @Test
     public void generatesEgyptCapitalCityLearnification() {
-        Randomiser stubRandomiser = new Randomiser() {
-            @Override
-            public String randomLearnificationQuestion(List<LearningItem> learningItems) {
-                return learningItems.get(0).left;
-            }
-        };
+        Randomiser stubRandomiser = learningItems -> learningItems.get(0).left;
         LearnificationRepository stubLearnificationRepository = mock(LearnificationRepository.class);
-        LearningItemTemplate learningItemTemplate = new LearningItemTemplate("What is the capital city of", "Which country has the capital city");
-        when(stubLearnificationRepository.learningItems()).thenReturn(Collections.singletonList(learningItemTemplate.build("Egypt", "Cairo")));
+        when(stubLearnificationRepository.learningItems()).thenReturn(Collections.singletonList(new LearningItem("Egypt", "Cairo")));
         LearnificationTextGenerator learnificationTextGenerator = new LearnificationTextGenerator(stubRandomiser, stubLearnificationRepository);
 
-        assertThat(learnificationTextGenerator.notificationText(), equalTo("What is the capital city of Egypt?"));
+        assertThat(learnificationTextGenerator.notificationText(), equalTo("Egypt"));
     }
 }
