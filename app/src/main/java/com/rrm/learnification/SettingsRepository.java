4498b33eb1d5aea2f03422c8b578d3ced4c9f192
romo <robert.moore@zuhlke.com>
2019-07-08 17:27:42 +0100

    Fix settings repository file interaction. This also needs an integration test!

--- app/src/main/java/com/rrm/learnification/SettingsRepository.java
+++ app/src/main/java/com/rrm/learnification/SettingsRepository.java
@@ -2,6 +2,7 @@ package com.rrm.learnification;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class SettingsRepository {
     private static final String LOG_TAG = "SettingsRepository";
@@ -30,14 +31,8 @@ class SettingsRepository {
 
     int readPeriodicitySeconds() {
         try {
-            List<String> lines = fileStorageAdaptor.readLines(PERIODICITY_FILE_NAME);
-            String firstLine = lines.get(0);
-            if (firstLine.isEmpty()) {
-                logger.v(LOG_TAG, "first line of periodicity file (" + PERIODICITY_FILE_NAME + ") was empty. Returning default periodicity " + DEFAULT_PERIODICITY_SECONDS + ")");
-                return DEFAULT_PERIODICITY_SECONDS;
-            }
-
-            return Integer.parseInt(firstLine.split("=")[1]);
+            List<String> lines = fileStorageAdaptor.readLines(PERIODICITY_FILE_NAME).stream().filter(line -> !line.isEmpty()).collect(Collectors.toList());
+            return Integer.parseInt(lines.get(0).split("=")[1]);
         } catch (Exception e) {
             logger.e(LOG_TAG, e);
             logger.v(LOG_TAG, "returning default periodicity in seconds (" + DEFAULT_PERIODICITY_SECONDS + ")");
