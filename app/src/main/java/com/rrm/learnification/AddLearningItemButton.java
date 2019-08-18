82a0d9b4af48eb1a70051bff1598bb66039e27c4
Rob Moore <robmoore121@gmail.com>
2019-08-18 12:07:05 +0100

    The add learning item button changes colour while your finger is down when you're pressing it, to give you some feedback that the tap is being registered.

--- app/src/main/java/com/rrm/learnification/AddLearningItemButton.java
+++ app/src/main/java/com/rrm/learnification/AddLearningItemButton.java
@@ -1,7 +1,10 @@
 package com.rrm.learnification;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 
 import java.util.ArrayList;
@@ -20,6 +23,7 @@ class AddLearningItemButton implements AndroidButton {
         this.logger = logger;
         this.button = addLearningItemView.addLearningItemButton();
         disable();
+        setOnFocusHandler();
     }
 
     @Override
@@ -33,7 +37,7 @@ class AddLearningItemButton implements AndroidButton {
     @Override
     public void enable() {
         activated = true;
-        button.getBackground().setColorFilter(Color.parseColor("#32CD32"), PorterDuff.Mode.MULTIPLY);
+        ButtonColour.setColour(button, ButtonColour.READY_TO_BE_CLICKED);
         button.setClickable(true);
         bindClickListenersToButton(onClickCommands);
     }
@@ -41,7 +45,7 @@ class AddLearningItemButton implements AndroidButton {
     @Override
     public void disable() {
         activated = false;
-        button.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY);
+        ButtonColour.setColour(button, ButtonColour.GRAYED_OUT);
         button.setClickable(false);
         bindClickListenersToButton(Collections.singletonList(OnClickCommand.doNothingOnClickCommand()));
     }
@@ -51,5 +55,29 @@ class AddLearningItemButton implements AndroidButton {
             logger.v(LOG_TAG, "add-learning-item-button clicked");
             onClickCommands.forEach(OnClickCommand::onClick);
         });
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setOnFocusHandler() {
+        button.setOnTouchListener((view, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN && activated) {
+                ButtonColour.setColour(view, ButtonColour.FINGER_DOWN);
+            } else if (event.getAction() == MotionEvent.ACTION_UP && activated) {
+                ButtonColour.setColour(view, ButtonColour.READY_TO_BE_CLICKED);
+            } else if (!activated) {
+                ButtonColour.setColour(view, ButtonColour.GRAYED_OUT);
+            }
+            return false;
+        });
+    }
+
+    private static class ButtonColour {
+        private static final int READY_TO_BE_CLICKED = Color.parseColor("#32CD32");
+        private static final int GRAYED_OUT = Color.GRAY;
+        private static final int FINGER_DOWN = Color.GREEN;
+
+        private static void setColour(View view, int colour) {
+            view.getBackground().setColorFilter(colour, PorterDuff.Mode.MULTIPLY);
+        }
     }
 }
