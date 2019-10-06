12ae8f423d7774afbfd92d4bc8bb4f36b358c388
Rob Moore <robmoore121@gmail.com>
2019-10-06 19:10:33 +0100

    optimise imports

--- app/src/main/java/com/rrm/learnification/notification/AndroidNotificationFactory.java
+++ app/src/main/java/com/rrm/learnification/notification/AndroidNotificationFactory.java
@@ -6,7 +6,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.RemoteInput;
