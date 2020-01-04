7b9f4151233f0657178f2f7a803a918511ad4ddb
Rob Moore <robmoore121@gmail.com>
2020-01-04 02:47:10 +0000

    Learnt some bash while measuring codebase size

---
+++ scripts/line-count.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+if [[ $1 != "androidTest" && $1 != "main" && $1 != "test" ]]
+then
+  echo "usage: $0 <main | androidTest | test>"
+  exit 1
+fi
+
+(find app/src/$1/java/com/rrm/learnification/ | grep "[.]java" | xargs -n 1 wc -l | cut -d ' ' -f1 | tr "\012" "+" ; echo "0") | bc
