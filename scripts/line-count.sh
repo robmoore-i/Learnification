4a3ea74003f4becc2d9d81282570a767cd50fdc4
romo <robert.moore@zuhlke.com>
2020-01-07 14:46:20 +0000

    Made script mac friendly

--- scripts/line-count.sh
+++ scripts/line-count.sh
@@ -6,4 +6,4 @@ then
   exit 1
 fi
 
-(find app/src/$1/java/com/rrm/learnification/ | grep "[.]java" | xargs -n 1 wc -l | cut -d ' ' -f1 | tr "\012" "+" ; echo "0") | bc
+(find app/src/$1/java/com/rrm/learnification/ | grep "[.]java" | xargs -n 1 wc -l | awk '{$1=$1};1' | cut -d ' ' -f1 | tr "\012" "+" ; echo "0") | bc
