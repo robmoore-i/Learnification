4d79ed26cafd4a5f59a760d09d922ac8a3726531
romo <robmoore121@gmail.com>
2020-05-16 17:04:29 +0800

    - Tighten access

--- app/src/main/java/com/rrm/learnification/learningitemseteditor/LearningItemStash.java
+++ app/src/main/java/com/rrm/learnification/learningitemseteditor/LearningItemStash.java
@@ -7,7 +7,7 @@ import com.rrm.learnification.textinput.AndroidTextWatcher;
 import com.rrm.learnification.textinput.OnTextChangeListener;
 import com.rrm.learnification.textlist.AndroidTextSource;
 
-public class LearningItemStash {
+class LearningItemStash {
     private static final String LOG_TAG = "LearningItemListCache";
     private final OnTextChangeListener onTextChangeListener;
     private final UpdatableLearningItemTextDisplayStash updatableLearningItemDisplayCache;
@@ -19,7 +19,7 @@ public class LearningItemStash {
         this.updatableLearningItemDisplayCache = updatableLearningItemDisplayCache;
     }
 
-    public void stash(EditText listItemView, String viewText, String textSourceId) {
+    void stash(EditText listItemView, String viewText, String textSourceId) {
         logger.i(LOG_TAG, "focus acquired on list item with text '" + viewText + "'");
         // Set update button to update the learning item based on viewText with the text of listItemView
         updatableLearningItemDisplayCache.saveText(new AndroidTextSource(listItemView), viewText);
@@ -27,7 +27,7 @@ public class LearningItemStash {
         onTextChangeListener.addTextSource(new AndroidTextWatcher(logger, textSourceId, listItemView));
     }
 
-    public void pop(EditText listItemView, String viewText, String textSourceId) {
+    void pop(EditText listItemView, String viewText, String textSourceId) {
         logger.i(LOG_TAG, "focus lost on list item with text '" + viewText + "'");
         // Reload text content from storage
         listItemView.setText(updatableLearningItemDisplayCache.savedText());
